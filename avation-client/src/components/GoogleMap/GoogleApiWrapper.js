b8e266870e5c1b0d05fb14f15a7aee4c57cef4d7
Mike Wu <mikejcwu@gmail.com>
2018-11-29 23:45:52 -0800

    Modifying google marker

--- avation-client/src/components/GoogleMap/GoogleApiWrapper.js
+++ avation-client/src/components/GoogleMap/GoogleApiWrapper.js
@@ -1,36 +1,34 @@
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import React, {Component} from 'react';
 import API_Key from '../api_key'
- 
+import AircraftIcon from './boeing-747-icon-256.png';
+
 export class MapContainer extends Component {
   render() {
     const style = {
       width: '75%',
       height: '75%'
     }
-    
+    const google = window.google
     return (
       <div style={style}>
         <Map google={this.props.google}
           style={style}
+          className={'map'}
+          zoom={11}
           initialCenter={{
             lat: 49.1967,
             lng: -123.1815
           }}
-          center={{
-            lat: 40.854885,
-            lng: -88.081807
-          }}
-          zoom={12}
-          onClick={this.onMapClicked}>
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />
-  
-          {/* <InfoWindow onClose={this.onInfoWindowClose}>
-              <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-              </div>
-          </InfoWindow> */}
+          >
+          <Marker
+            name={'Your position'}
+            position={{lat: 49.1967, lng: -123.1815}}
+            icon={{ 
+              url: AircraftIcon, // url
+              scaledSize: new google.maps.Size(22, 22), // scaled size
+              anchor: new google.maps.Point(10, 10) // anchor
+             }} />
         </Map>
       </div>
     );
@@ -39,4 +37,5 @@ export class MapContainer extends Component {
  
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
+  // apiKey: API_Key.googleMapAPI
 })(MapContainer)
